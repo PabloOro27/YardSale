982f6a4960d44fa023a00498024e028cc190f622
PabloOro27 <pabloorozco2712@gmail.com>
2023-05-03 22:49:24 -0600

    uso de APIs para ingresar productos

--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import useGetProducts from '../hooks/useGetProducts';
 import ProductItem from '../components/ProductItem';
 import '../styles/ProductList.css'; 
 
+// recursos de apis
+const API = 'https://api.escuelajs.co/api/v1/products'; 
+
 const ProductList = () => {
+    const products = useGetProducts(API);
+    
     return (
-    <section className="main-container">
-        <div className="ProductList">
-            <ProductItem />
-        </div>
-    </section>
-    );
+		<section className="main-container">
+			<div className="ProductList">
+				{products.map(product => (
+					<ProductItem product = {product} key = {product.id} />
+				))}
+			</div>
+		</section>
+	);
 };
 
 export default ProductList;
