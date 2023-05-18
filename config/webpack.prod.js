26e3bfe5d4e22afc6bc20339b3983ad030373b94
PabloOro27 <pabloorozco2712@gmail.com>
2023-05-17 21:54:57 -0600

    [fix] configuracion webpack production

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,11 +1,26 @@
-// archivo de configuracion para produccion 
-const { merge } = require("webpack-merge"); //se realiza el merge de las configuraciones 
-const common = require("./webpack.common"); //se llama el archivo comun 
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common");
 
 /** @type {import('webpack').Configuration} */
-
 const prodConfig = {
-    mode: "production",
+  mode: "production",
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.(css|scss|sass)$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  optimization: {
+    splitChunks: {
+      chunks: "all",
+      name: false,
+    },
+  },
+  plugins: [new MiniCssExtractPlugin()],
 };
 
 module.exports = merge(common, prodConfig);
